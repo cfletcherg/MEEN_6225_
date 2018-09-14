f2deed69d9918d07a713f49255c0351e10e9ffab
cfletcherg <f.garrison4@gmail.com>
2018-09-14 00:01:57 -0600

    First Commit

---
+++ run_graph_search.py
@@ -0,0 +1,10 @@
+import graph_search
+
+def run_dfs(map_path):
+    g = graph_search.GridMap(map_path)
+    res = graph_search.dfs(g.init_pos, g.transition, g.is_goal, graph_search._ACTIONS)
+    g.display_map(res[0][0], res[1])
+
+if __name__ == '__main__':
+    run_dfs('./map1.txt')
+
